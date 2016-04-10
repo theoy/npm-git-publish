50870dc2274b187d8d98a68196fbc5c8778ceff5
Theo Yaung <theoy@outlook.com>
2016-04-11 00:58:57 +0100

    Implement support for optional parameters/default values
    Also refactor the signature some, in anticipation of future functionality.
    
    Resolves #2.

--- lib/publish.ts
+++ lib/publish.ts
@@ -7,28 +7,83 @@ import mkdirp from './wrappers/mkdirp'
 import rimraf from './wrappers/rimraf';
 import unpack from './unpack';
 
-type PackageInfo = { name: string, version: string };
+interface PackageInfo {
+    name: string;
+    version: string
+}
 
-export default function publish(packageDir: string,
-                                gitRemoteUrl: string,
-                                commitText: string,
-                                tagName: string,
-                                tagMessageText: string,
-                                tempDir: string,
-                                packageInfo: PackageInfo): Promise<boolean> {
+interface Options {
+    commitText?: string;
+    tagName?: string;
+    tagMessageText?: string;
+    tempDir?: string;
+    packageInfo?: PackageInfo;
+}
 
+interface Params {
+    commitText: string;
+    tagName: string;
+    tagMessageText: string;
+    tempDir: string;
+    packageInfo: PackageInfo;
+}
+
+interface Result {
+    conclusion: 'pushed' | 'cancelled' | 'skipped'
+}
+
+// new overload
+export default function publishExport(packageDir: string,
+    gitRemoteUrl: string,
+    options?: Options): Promise<Result>;
+
+// old deprecated overload
+export default function publishExport(packageDir: string,
+    gitRemoteUrl: string,
+    commitText: string,
+    tagName: string,
+    tagMessageText: string,
+    tempDir: string,
+    packageInfo: PackageInfo): Promise<boolean>;
+
+export default function publishExport(packageDir: string,
+    gitRemoteUrl: string,
+    options?: string | Options,
+    tagName?: string,
+    tagMessageText?: string,
+    tempDir?: string,
+    packageInfo?: PackageInfo): Promise<boolean | Result> {
+
+    if (typeof options === 'string') {
+        // using the deprecated overload
+        return publish(packageDir, gitRemoteUrl, {
+            commitText: options,
+            tagName: tagName,
+            tagMessageText: tagMessageText,
+            tempDir: tempDir,
+            packageInfo: packageInfo
+        })
+            .then(result => result.conclusion === 'pushed');
+    } else {
+        // otherwise assume they want the new overload
+        return provideDefaults(packageDir, gitRemoteUrl, options)
+            .then(params => publish(packageDir, gitRemoteUrl, params));
+    }
+}
+
+function publish(packageDir: string, gitRemoteUrl: string, params: Params): Promise<Result> {
     const writeFile = pify(fs.writeFile),
-        gitRepoDir = path.join(tempDir, 'repo'),
-        packDir = path.join(tempDir, 'pack'),
-        commitTextPath = path.join(tempDir, 'commitMessage.txt'),
-        tagTextPath = path.join(tempDir, 'tagMessage.txt'),
+        gitRepoDir = path.join(params.tempDir, 'repo'),
+        packDir = path.join(params.tempDir, 'pack'),
+        commitTextPath = path.join(params.tempDir, 'commitMessage.txt'),
+        tagTextPath = path.join(params.tempDir, 'tagMessage.txt'),
         cleanupOperations: Promise<any>[] = [];
 
     // launch setup operations
-    const initialCleanDone = rimraf(tempDir, { glob: false });
+    const initialCleanDone = rimraf(params.tempDir, { glob: false });
     const directoryReady = initialCleanDone.then(() => mkdirp(packDir));
-    const commitTextWritten = directoryReady.then(() => writeFile(commitTextPath, commitText));
-    const tagTextWritten = directoryReady.then(() => writeFile(tagTextPath, tagMessageText));
+    const commitTextWritten = directoryReady.then(() => writeFile(commitTextPath, params.commitText));
+    const tagTextWritten = directoryReady.then(() => writeFile(tagTextPath, params.tagMessageText));
 
     // simultaneously ask NPM to pack up the package dir and create a clone of the remote URL
     const tarballCreated = packPackageIntoTarball();
@@ -48,8 +103,8 @@ export default function publish(packageDir: string,
             }
 
             function cleanUpAndReturnChanged() {
-                cleanupOperations.push(rimraf(tempDir, { glob: false }));
-                return Promise.all(cleanupOperations).then(() => hasChanges);
+                cleanupOperations.push(rimraf(params.tempDir, { glob: false }));
+                return Promise.all(cleanupOperations).then(() => ({ conclusion: hasChanges ? 'pushed' : 'skipped' }));
             }
         });
 
@@ -61,7 +116,7 @@ export default function publish(packageDir: string,
             .then(() => exec('npm pack "' + packageDir + '"', { cwd: packDir }))
             .then((packCommandOutput) => {
                 // pack succeeded! Schedule a cleanup and return the full path
-                cleanupOperations.push(exec('npm cache clean ' + packageInfo.name + '@' + packageInfo.version));
+                cleanupOperations.push(exec('npm cache clean ' + params.packageInfo.name + '@' + params.packageInfo.version));
                 const packFileName = packCommandOutput.replace(/\r\n|\r|\n/g, '');
                 return path.join(packDir, packFileName);
             });
@@ -83,8 +138,8 @@ export default function publish(packageDir: string,
 
         const doneCleaning = doneCloning.then(() => rimraf(cleanPattern, cleanOptions));
 
-        return Promise.all<any>([ tarballCreated, doneCleaning ])
-            .then((results: [string]) => unpack(results[ 0 ], gitRepoDir));
+        return Promise.all<any>([tarballCreated, doneCleaning])
+            .then((results: [string]) => unpack(results[0], gitRepoDir));
     }
 
     function stageAllRepoChanges() {
@@ -101,12 +156,35 @@ export default function publish(packageDir: string,
     }
 
     function tagLastCommit() {
-        const tagCommandText = 'git tag --annotate --file="'+tagTextPath+'" "'+tagName+'"';
+        const tagCommandText = 'git tag --annotate --file="' + tagTextPath + '" "' + params.tagName + '"';
         return tagTextWritten.then(() => exec(tagCommandText, { cwd: gitRepoDir }));
     }
 
     function pushDefaultBranch() {
         execSync('git push --follow-tags origin HEAD', { cwd: gitRepoDir, stdio: 'inherit' });
+    }
+}
+
+function provideDefaults(packageDir: string, gitRemoteUrl: string, options?: Options) : Promise<Params> {
+    options = options || {};
+
+    if (options.packageInfo) {
+        return Promise.resolve(provideRemainingDefaults(options.packageInfo));
+    } else {
+        const readPkg = require('read-pkg') as ((packageDir: string) => Promise<PackageInfo>);
+        return readPkg(packageDir).then(provideRemainingDefaults);
+    }
+
+    function provideRemainingDefaults(packageInfo: PackageInfo) : Options {
+        const version = packageInfo.version,
+            commitText = options.commitText || 'release: version ' + version;
+
+        return {
+            commitText: commitText,
+            tagMessageText: options.tagMessageText || commitText,
+            tagName: options.tagName || 'v' + version,
+            tempDir: options.tempDir || require('unique-temp-dir')()
+        };
     }
 }
 
